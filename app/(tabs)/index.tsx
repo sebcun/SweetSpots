896e97b16139150214497d69d0a9834be81e6429
Sebastian Cunningham <80603287+sebcun@users.noreply.github.com>
2025-10-28 11:58:21 +1100

    Added map view

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,10 @@
-import { StyleSheet } from "react-native";
-
-import { Text, View } from "@/components/Themed";
+import { StyleSheet, View } from "react-native";
+import MapView from "react-native-maps";
 
 export default function MapScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
+      <MapView style={styles.map} />
     </View>
   );
 }
@@ -18,16 +12,9 @@ export default function MapScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
   },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
+  map: {
+    width: "100%",
+    height: "100%",
   },
 });
